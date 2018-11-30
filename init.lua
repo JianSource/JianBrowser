22e7bb1e3f3a764f17496a781d7a68d199d6c0a4
LTDSA <36990008+LTDSA@users.noreply.github.com>
2018-11-30 22:05:00 +0800

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,13 @@
+appname="简浏览器"
+appver="3.0.0"
+packagename="cn.ltdsa.jianbrowser"
+template="tool"
+appcode="7"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
